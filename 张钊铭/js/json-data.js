88c035b58ba3b490b5755a89436ffa4ea02d620b
Selection <745989385@qq.com>
2017-10-10 09:59:12 +0800

    shijuezhongguo

---
+++ 张钊铭/js/json-data.js
@@ -0,0 +1,14 @@
+var json={
+	"login":[{
+		"username":"liufeng",
+		"password":"12345",
+	},{
+		"username":"zhangzhaoming",
+		"password":"366710",
+
+	},{
+		"username":"wuxionghui",
+		"password":"45678",
+	}]
+	"ars":[{},{},{},]
+}
